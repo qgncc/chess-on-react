08c0f7bd2f6569839baf30b76008878857a91b38
0xEVG <evgenikrolikov@gmail.com>
2022-07-27 21:00:22 +0700

    yep

--- src/hooks/useChessGameManager.ts
+++ src/hooks/useChessGameManager.ts
@@ -7,8 +7,20 @@ type OutgoingMessage = {type: "move", move: string}
                         |{type: "game_ended", reason: string}
 
 export function useChessWebSocket(url: string){
+    
+    
+    
+    
     function onMessage(message: IncomingMessage) {
         
     }
     const ws = useWebSocket<IncomingMessage, OutgoingMessage>(url, onMessage)
+
+
+
+
+
+    return{
+
+    }
 }
